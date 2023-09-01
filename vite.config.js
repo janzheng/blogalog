76632a28f3c405f4ef5ef73d909f498f83cfc115
Jan Zheng <janzheng@Jans-CrazyBook-Pro.local>
2023-09-01 15:46:06 +1000

    oops

--- vite.config.js
+++ vite.config.js
@@ -34,8 +34,8 @@ const config = {
       $instill: path.resolve('./src/routes/instill'),
 
       
-      // $plasmid: process.env.PUBLIC_LOCAL == 'local' ? path.resolve('./src/plasmid') : path.resolve('./node_modules/plasmid'), // dynamic linked
-      $plasmid: path.resolve('./src/plasmid'), // local linked
+      $plasmid: process.env.PUBLIC_LOCAL == 'local' ? path.resolve('./src/plasmid') : path.resolve('./node_modules/plasmid'), // dynamic linked
+      // $plasmid: path.resolve('./src/plasmid'), // local linked
       // $plasmid: path.resolve('./node_modules/plasmid'), // git linked
 
       '$instill-helpers': process.env.PUBLIC_LOCAL == 'local' ? path.resolve('./src/plasmid/modules/instill-helpers') : path.resolve('./node_modules/plasmid/modules/instill-helpers'), // dynamic linked
